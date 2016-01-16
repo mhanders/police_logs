61b6e705ea8d0748e33a757e07fa8ed8dcfbcbb3
Michael Handley <handley@mit.edu>
2016-01-16 16:17:48 -0500

    Add some logging

--- data_collection/download_and_serialize_logs.py
+++ data_collection/download_and_serialize_logs.py
@@ -3,10 +3,12 @@ from os import listdir, path
 from subprocess import check_output
 from log_parser import LogParser
 from police_logs import HarvardPoliceLog
+import requests
+import logging
 
 TEMP_PDF_DIR = 'temp_data/'
 
-HarvardPDFDownloader().run(TEMP_PDF_DIR + '%s')
+HarvardPDFDownloader().run(TEMP_PDF_DIR)
 pdf_names = listdir(TEMP_PDF_DIR)
 if len(pdf_names) > 0:
     log_texts = [check_output(['pdftotext', path.join(TEMP_PDF_DIR, pdf_name), '-']) for pdf_name in pdf_names
@@ -14,3 +16,6 @@ if len(pdf_names) > 0:
     lp = LogParser(HarvardPoliceLog)
     unflattened_police_logs = [lp.parse(text=log_text) for log_text in log_texts]
     police_logs = [log for log_list in unflattened_police_logs for log in log_list]
+    for log_json in map(lambda log: log.to_json(), police_logs):
+        result = requests.post('http://localhost:8000/internal/create/', data=log_json)
+        logging.debug("Got %d after POSTing %s" % (result.status_code, log_json))
